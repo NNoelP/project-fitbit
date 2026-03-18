c389c194a5b53605cea6e34e306cc89497a79783
npandayVU <noelpanday95@gmail.com>
2026-03-18 22:38:36 +0100

    More heart_rate stats

--- module.py
+++ module.py
@@ -34,36 +34,9 @@ class StdevAgg:
             return 0.0
         return np.std(self.values, ddof=1)  # sample stdev
 
-class MinDateAgg:
-    def __init__(self):
-        self.values = []
-
-    def step(self, v):
-        if v is not None:
-            self.values.append(v)
-
-    def finalize(self):
-        date = pd.to_datetime(self.values, format=DATE_FMT).sort_values()[0]
-        return str(date)
-
-class MaxDateAgg:
-    def __init__(self):
-        self.values = []
-
-    def step(self, v):
-        if v is not None:
-            self.values.append(v)
-
-    def finalize(self):
-        date = pd.to_datetime(self.values, format=DATE_FMT).sort_values()[-1]
-        return str(date)
-
 conn = connect("./data/fitbit_database.db")
 conn.create_aggregate("percentile", 2, PercentileAgg)
 conn.create_aggregate("stdev", 1, StdevAgg)
-conn.create_aggregate("min_date", 1, MinDateAgg)
-conn.create_aggregate("max_date", 1, MaxDateAgg)
-conn.create_function("to_date", 1, lambda x: str(pd.to_datetime(x, format=DATE_FMT)))
 
 def table_info(table):
     nrows = pd.read_sql(f"SELECT COUNT(*) FROM {table}", conn).values[0,0]
