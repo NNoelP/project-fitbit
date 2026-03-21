14a2175fcaafff7bae760d4b19830ee5caaadea2
sevvalyelmer <sevvalbaharyelmer@gmail.com>
2026-03-21 13:53:56 +0100

    Sevval - Distance per user

--- fitbit.py
+++ fitbit.py
@@ -15,3 +15,27 @@ daily_activity["ActivityDate"] = pd.to_datetime(daily_activity["ActivityDate"])
 def unique_users(df):
     unique_users = df["Id"].nunique()
     st.write(f"Total unique users: {unique_users}")
+
+
+############## PART 1
+
+# TOTAL DISTANCE PER USER
+
+
+def distance_per_user(df):
+    total_distance_per_user = (
+        df.groupby("Id")["TotalDistance"].sum().sort_values(ascending=False)
+    )
+
+    plt.figure(figsize=(12, 6))
+    sns.barplot(
+        x=total_distance_per_user.index, y=total_distance_per_user.values
+    )  # using a barplot
+    plt.xticks(rotation=90)  # without adding this ID's were not readable
+    plt.title("Total Distance Registered for Each User")
+    plt.xlabel("User ID")
+    plt.ylabel("Total Distance")
+    plt.tight_layout()
+    plt.show()
+    st.pyplot(plt.gcf())
+    plt.close()
